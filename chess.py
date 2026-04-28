a5752e5efd17a9579ee912233cb5dad20bfb347a
eevehu <eevehu@gmail.com>
2026-04-29 00:12:00 +0100

    Started implemented pawn logic

--- chess.py
+++ chess.py
@@ -41,7 +41,18 @@ def boardSetUp():
 
     return board
 
-# def pawnMovement(startPoint, endPoint):
+def pawnMovement(startPoint, endPoint, board, attacking):
+    # Work on en-passant later
+
+    piece = board[f"{endPoint}"]
+    if attacking:
+        if int(endPoint[1]) - int(startPoint[1])!= 1:
+            return False
+        index = lateral.find(startPoint[0])
+        print(index)
+        # Implement checking for that the attacks are in range!!! 
+        # Also, probably move this code to a different function that each of the pieces make use of
+        # It increases coupling... Maybe don't idk
 
 
 def userMove(board, color):
@@ -49,10 +60,12 @@ def userMove(board, color):
     # Based on that I can figure out the piece & and if its color matchs the person moving
     # And then if the position they're moving to is a valid one for that piece/isn't filled
     # I.E. A valid move position.
+    board[f"B3"] = ["p", "black"] # for cheking pawn attacking
 
     format_error = "Incorrect Format"
     new_positions = []
     while (True):
+        taking = False
         input_positions = input("Example: H4, A4\n").strip().split(", ")
         for position in input_positions:
             try:
@@ -74,29 +87,34 @@ def userMove(board, color):
             print("Square selected to move is empty")
             continue
         elif len(board[f"{new_positions[1]}"]) == 2:
+            taking = True
             if board[f"{new_positions[1]}"][1] == color:
                 print("Unable to move piece onto your own piece")
                 continue
         
         print(f"Moving from {input_positions[0]} to {input_positions[1]}")
-        break
+
     
-    piece = board[f"{new_positions[0]}"][0]
-    if piece == pieces[0]:
-        print("Selected square is a Pawn")
-        pawnMovement(new_positions[0], new_positions[1])
-    elif piece == pieces[1]:
-        print("Selected square is a Knight")
-    elif piece == pieces[2]:
-        print("Selected square is a Bishop")
-    elif piece == pieces[3]:
-        print("Selected square is a Rook")
-    elif piece == pieces[4]:
-        print("Selected square is a Queen")
-    elif piece == pieces[5]:
-        print("Selected square is a King")
-    else:
-        print("Somehow the selected piece is.... Not a piece? Weird, huh")
+        piece = board[f"{new_positions[0]}"][0]
+        if piece == pieces[0]:
+            print("Selected square is a Pawn")
+            if pawnMovement(new_positions[0], new_positions[1], board, taking) == False:
+                print("Pawn is unable to move to that position")
+                continue
+            else:
+                return
+        elif piece == pieces[1]:
+            print("Selected square is a Knight")
+        elif piece == pieces[2]:
+            print("Selected square is a Bishop")
+        elif piece == pieces[3]:
+            print("Selected square is a Rook")
+        elif piece == pieces[4]:
+            print("Selected square is a Queen")
+        elif piece == pieces[5]:
+            print("Selected square is a King")
+        else:
+            print("Somehow the selected piece is.... Not a piece? Weird, huh")
 
 
 while (True):
