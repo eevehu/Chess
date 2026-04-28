6eda82b19c925fa3ea5d30f3476a84783ee68078
eevehu <eevehu@gmail.com>
2026-04-28 21:14:21 +0100

    Added comments for ideas about the project

---
+++ chess.py
@@ -0,0 +1,8 @@
+# Would it be better to store the different colored squares as two different arrays/lists?
+
+# Dictionary for h4 = [7, 3]???
+# Set up the Dictionary with a nested for loop assigning letters from a string to values
+
+lateral = "ABCDEFGH"
+for letter in lateral:
+    print(letter)
